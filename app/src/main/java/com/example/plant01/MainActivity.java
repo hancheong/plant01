ba7ac674d190f784c31b0b90105b11317bb61154
kmijin <93758294+kmijin@users.noreply.github.com>
2021-11-18 15:32:34 +0900

    주석삭제

--- app/src/main/java/com/example/plant01/MainActivity.java
+++ app/src/main/java/com/example/plant01/MainActivity.java
@@ -7,7 +7,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 
-//0000
+
 public class MainActivity extends AppCompatActivity
 {
    RecyclerView recview;

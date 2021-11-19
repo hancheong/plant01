64c3dacb51c915a50ee1400b487960fd3cc0740a
hancheong <rhrnak0708@gmail.com>
2021-11-19 13:50:04 +0900

    no message

--- app/src/main/java/com/example/plant01/home/Home.java
+++ app/src/main/java/com/example/plant01/home/Home.java
@@ -1,10 +1,13 @@
 package com.example.plant01.home;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.example.plant01.R;
 import com.example.plant01.adaptor.MyAdapter;
@@ -21,6 +24,8 @@ import com.smarteist.autoimageslider.SliderView;
 
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 

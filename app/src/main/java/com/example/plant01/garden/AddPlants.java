6d51347a45ef39793c7fa786ec29f7c969e1744d
Dnarbon <yng05084@g.shingu.ac.kr>
2021-11-19 15:49:26 +0900

    AddPlants.class 추가

---
+++ app/src/main/java/com/example/plant01/garden/AddPlants.java
@@ -0,0 +1,45 @@
+package com.example.plant01.garden;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.plant01.R;
+
+public class AddPlants extends AppCompatActivity {
+
+    EditText etName, etLocation, etDate;
+    TextView tvTitle;
+    Button btnBack, btnSave;
+    View.OnClickListener cl;
+    Intent in;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_plants);
+
+        etName = (EditText) findViewById(R.id.etName);
+        etLocation = (EditText) findViewById(R.id.etLocation);
+        etDate = (EditText) findViewById(R.id.etDate);
+
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnBack = (Button) findViewById(R.id.btnBack);
+
+        cl = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        };
+        btnSave.setOnClickListener(cl);
+        btnBack.setOnClickListener(cl);
+    }
+}

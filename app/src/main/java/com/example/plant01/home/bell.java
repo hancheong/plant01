951452e0994ed49352438a7dad232791fbe71ce7
cheong <rhrnak0708@g.shingu.ac.kr>
2021-11-22 21:48:41 +0900

    Intent로 드로워 선택 넘김

--- app/src/main/java/com/example/plant01/home/bell.java
+++ app/src/main/java/com/example/plant01/home/bell.java
@@ -1,14 +1,14 @@
-package com.example.plant01.usersetting;
+package com.example.plant01.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class UserSetting extends AppCompatActivity {
+public class bell extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_user_setting);
+        setContentView(R.layout.activity_bell);
     }
 }

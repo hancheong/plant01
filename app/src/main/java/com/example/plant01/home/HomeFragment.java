fcd8ae60af948abc7d1fb459c3c4c8cd2b23012f
cheong <rhrnak0708@g.shingu.ac.kr>
2021-11-22 22:02:11 +0900

    로그인 버튼 정렬

--- app/src/main/java/com/example/plant01/home/HomeFragment.java
+++ app/src/main/java/com/example/plant01/home/HomeFragment.java
@@ -92,14 +92,24 @@ public class HomeFragment extends Fragment {
         sliderView.setSliderTransformAnimation(SliderAnimations.DEPTHTRANSFORMATION);
         sliderView.startAutoCycle();
 
-/*----------------추천상품 부분 -------------------------------*/
+
+
+
+        /*----------------추천상품 부분 -------------------------------*/
+
         recyclerView = getView().findViewById(R.id.recyclerView);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), RecyclerView.HORIZONTAL, false));
+        plantsArrayList = new ArrayList<Plants>();
+        plantAdapter = new home_PlantAdapter(getContext(), plantsArrayList);
+        recyclerView.setAdapter(plantAdapter);
+        db = FirebaseFirestore.getInstance();
+        showRecomendPlant();
 
 
 
-/*------------------드로워부분-------------------*/
+        /*------------------드로워부분-------------------*/
+
         final DrawerLayout drawerLayout1 = (DrawerLayout) getView().findViewById(R.id.drawerLayout);
         NavigationView navigationView = getView().findViewById(R.id.nv_homedrawer);
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
@@ -113,46 +123,18 @@ public class HomeFragment extends Fragment {
                     case R.id.btn_bell:
                         Intent intent2 = new Intent(getActivity(), bell.class);
                         startActivity(intent2);
-
                         break;
-
-
                 }
                 return true;
             }
         });
 
 
-
-
-
-//        getView().findViewById(R.id.imagemenu).setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                drawerLayout1.openDrawer(GravityCompat.START);
-//            }
-//        });
-
-
-
-
-        plantsArrayList = new ArrayList<Plants>();
-        plantAdapter = new home_PlantAdapter(getContext(), plantsArrayList);
-
-        recyclerView.setAdapter(plantAdapter);
-        db = FirebaseFirestore.getInstance();
-
-
-        showRecomendPlant();
-
-
-
-
-/*--------- 클릭 이벤트 -------------*/
+        /*--------- 클릭 이벤트 -------------*/
         cl = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                switch (v.getId()){
+                switch (v.getId()) {
                     case R.id.imagemenu: //햄버거 버튼
                         showUserProfile();
                         drawerLayout1.openDrawer(GravityCompat.START);
@@ -165,9 +147,8 @@ public class HomeFragment extends Fragment {
     }
 
 
-
     /*------------------추천상품 보여주는 부분----------------------------------*/
-    public  void  showRecomendPlant() {
+    public void showRecomendPlant() {
         db.collection("Plants").addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
             public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
@@ -185,14 +166,13 @@ public class HomeFragment extends Fragment {
     }
 
 
-
     /*-----------------드로워 유저 프로필 보여주는 부분분---------------------------*/
-    public void showUserProfile(){
+    public void showUserProfile() {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         //부분업데이트?어떻게
         CircleImageView userprofile = getView().findViewById(R.id.iv_userProfile);
         TextView usernick = getView().findViewById(R.id.tv_userNick);
-        db.collection("Users").document("Qord9WQq0KW3frMfpzZy6ASgnCw1").get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+        db.collection("Users").document(user.getUid()).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                 if (task.isSuccessful()) {
@@ -208,56 +188,12 @@ public class HomeFragment extends Fragment {
                 }
             }
 
-        } );
+        });
 
 
+    }
+}
 
-}}
-
-//    private void EventChangeListener() {
-//        db.collection("Plants").addSnapshotListener(new EventListener<QuerySnapshot>() {
-//            @Override
-//            public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
-//
-//                plantsArrayList.clear();
-//                for (DocumentChange dc : value.getDocumentChanges()) {
-//                    if (dc.getType() == DocumentChange.Type.ADDED) {
-//                        plantsArrayList.add(dc.getDocument().toObject(Plants.class));
-//
-//                    }
-//                    plantAdapter.notifyDataSetChanged();
-//
-//                }
-//            }
-//        });
-//    }
-
-
-
-
-
-//    public void getPlant()
-//    {
-//        databaserf.addListenerForSingleValueEvent(new ValueEventListener()
-//        {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                // 파이어베이스 데이터베이스의 데이터를 받아오는 곳
-//                plantsArrayList.clear(); // 기존 배열리스트가 존재하지않게 초기화
-//                for (DataSnapshot snapshot : dataSnapshot.getChildren()) { // 반복문으로 데이터 List를 추출해냄
-//                    Plants plant = snapshot.getValue(Plants.class); // 만들어뒀던 Users 객체에 데이터를 담는다.
-//                    plantsArrayList.add(plant); // 담은 데이터들을 배열리스트에 넣고 리사이클러뷰로 보낼 준비
-//                }
-//                plantAdapter.notifyDataSetChanged(); // 리스트 저장 및 새로고침
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//                // 디비를 가져오던중 에러 발생 시
-//                Log.e("MainActivity", String.valueOf(databaseError.toException())); // 에러문 출력
-//            }
-//        });
-//    }
 
 
 

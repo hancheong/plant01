03a30b8b937e06f1af32847afd213cea38c94fa1
cheong <rhrnak0708@g.shingu.ac.kr>
2021-11-22 21:49:18 +0900

    intent로 드로워 선택 넘김  스토어나 다른부부은 이전 삽질방법으로 하면될듯함

--- app/src/main/java/com/example/plant01/home/bell.java
+++ app/src/main/java/com/example/plant01/home/bell.java
@@ -1,14 +1,38 @@
 package com.example.plant01.home;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.plant01.R;
 
 public class bell extends AppCompatActivity {
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_bell);
+        setContentView(R.layout.home_setting_bell);
+        toolbar = findViewById(R.id.toolbar2);
+
+        setSupportActionBar(toolbar);
+        setTitle("알림설정");
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return  true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/plant01/usersetting/UserSetting.java
+++ app/src/main/java/com/example/plant01/usersetting/UserSetting.java
@@ -1,14 +1,39 @@
 package com.example.plant01.usersetting;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.plant01.R;
 
 public class UserSetting extends AppCompatActivity {
+
+
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home_user_setting);
+        setContentView(R.layout.home_setting_user);
+        toolbar = findViewById(R.id.toolbar2);
+
+        setSupportActionBar(toolbar);
+        setTitle("알림설정");
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                onBackPressed();
+                return  true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }

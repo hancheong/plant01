235a346832996834e8dec04cb90f21f0944e0137
jinmin <wlsalsdla1@g.shingu.ac.kr>
2021-12-03 00:12:02 +0900

    검색결과 db 주석처리

--- app/src/main/java/com/example/plant01/store/StoreSearchResult.java
+++ app/src/main/java/com/example/plant01/store/StoreSearchResult.java
@@ -52,31 +52,31 @@ public class StoreSearchResult extends AppCompatActivity {
         if (name != null)
             result.setText(name);
 
-        showData();
+//        showData();
 
     }
 
-    private void showData() {
-
-        Intent intent = getIntent();
-
-        db.collection("StoreGoods")
-                .whereEqualTo("goodsKind", intent.getStringExtra("contact_search"))
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d(TAG, document.getId() + " => " + document.getData());
-                            }
-                        } else {
-                            Log.d(TAG, "Error getting documents: ", task.getException());
-                        }
-                    }
-                });
-
-
-    }
+//    private void showData() {
+//
+//        Intent intent = getIntent();
+//
+//        db.collection("StoreGoods")
+//                .whereEqualTo("goodsKind", intent.getStringExtra("contact_search"))
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                Log.d(TAG, document.getId() + " => " + document.getData());
+//                            }
+//                        } else {
+//                            Log.d(TAG, "Error getting documents: ", task.getException());
+//                        }
+//                    }
+//                });
+//
+//
+//    }
 }
 

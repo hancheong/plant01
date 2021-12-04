73816bb1eeb9b0adf616ea66109919383a1aa070
jinmin <wlsalsdla1@g.shingu.ac.kr>
2021-12-04 20:01:00 +0900

    스토어 세부 조정

--- app/src/main/java/com/example/plant01/store/Store_RecommendList.java
+++ app/src/main/java/com/example/plant01/store/Store_RecommendList.java
@@ -59,8 +59,13 @@ public class Store_RecommendList extends AppCompatActivity {
     ImageButton store_back, img1, img2, img3, img4, img5;
     private RecyclerView recyclerView;
     private FirebaseFirestore db;
-    private store_GoodsAdapter store_goodsAdapter;
+    private store_GoodsAdapter2 store_goodsAdapter2;
     private List <StoreGoods> list;
+    private ImageButton store_resultback;
+    private store_GoodsAdapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
 
     TextView store1, title1, review1, price1;
     TextView store2, title2, review2, price2;
@@ -82,16 +87,14 @@ public class Store_RecommendList extends AppCompatActivity {
             }
         });
 
-
-
         recyclerView = findViewById(R.id.store_recycle);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         db = FirebaseFirestore.getInstance();
         list = new ArrayList<>();
-//        store_goodsAdapter = new store_GoodsAdapter(this, list);
-        recyclerView.setAdapter(store_goodsAdapter);
+        store_goodsAdapter2 = new store_GoodsAdapter2(this, list);
+        recyclerView.setAdapter(store_goodsAdapter2);
 
         showData();
 
@@ -99,160 +102,26 @@ public class Store_RecommendList extends AppCompatActivity {
 
     private void showData() {
 
-        img1 = (ImageButton) findViewById(R.id.recommend_img1);
-        store1 = (TextView) findViewById(R.id.recommend_store1);
-        title1 = (TextView) findViewById(R.id.recommend_title1);
-        review1 = (TextView) findViewById(R.id.recommend_review1);
-        price1 = (TextView) findViewById(R.id.recommend_price1);
-
-        store2 = (TextView) findViewById(R.id.recommend_store2);
-        title2 = (TextView) findViewById(R.id.recommend_title2);
-        review2 = (TextView) findViewById(R.id.recommend_review2);
-        price2 = (TextView) findViewById(R.id.recommend_price2);
-
-        store3 = (TextView) findViewById(R.id.recommend_store3);
-        title3 = (TextView) findViewById(R.id.recommend_title3);
-        review3 = (TextView) findViewById(R.id.recommend_review3);
-        price3 = (TextView) findViewById(R.id.recommend_price3);
-
-        store4 = (TextView) findViewById(R.id.recommend_store4);
-        title4 = (TextView) findViewById(R.id.recommend_title4);
-        review4 = (TextView) findViewById(R.id.recommend_review4);
-        price4 = (TextView) findViewById(R.id.recommend_price4);
-
-        store5 = (TextView) findViewById(R.id.recommend_store5);
-        title5 = (TextView) findViewById(R.id.recommend_title5);
-        review5 = (TextView) findViewById(R.id.recommend_review5);
-        price5 = (TextView) findViewById(R.id.recommend_price5);
-
-// 첫번째 상품
-        DocumentReference goods1 = db.collection("StoreGoods").document("8hIJAMJMC9WA0sw7hKp8");
-        goods1.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-//                        String storeImg1 = (String) document.get("goodsImg");  // 상품 이미지 지정
-//                        img1.getImage(storeImg1);
-                        String storeName1 = (String) document.get("StoreName");  // 스토어 이름 지정
-                        store1.setText(storeName1);
-                        String goodsTitle1 = (String) document.get("GoodsTitle"); // 상품 제목 지정
-                        title1.setText(goodsTitle1);
-                        String goodsReview1 = (String) document.get("GoodsReview"); // 상품 리뷰 지정
-                        review1.setText(goodsReview1);
-                        String goodsPrice1 = (String) document.get("GoodsPrice"); // 상품 가격 지정
-                        price1.setText(goodsPrice1);
-                    } else {
-                        Log.d(TAG, "No such document"); // 문서를 못찾았을 경우
+        // 상품 데이터 객체 생성
+        db.collection("StoreGoods").get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        list.clear();
+                        for (DocumentSnapshot snapshot : task.getResult()){
+                            StoreGoods storeGoods = new StoreGoods(snapshot.getString("goodsImg"),snapshot.getString("StoreName"),snapshot.getString("GoodsTitle"),snapshot.getString("GoodsReview"),snapshot.getString("GoodsPrice"));
+                            list.add(storeGoods);
+                        }
+                        store_goodsAdapter2.notifyDataSetChanged();
                     }
-                } else {
-                    Log.d(TAG, "get failed with ", task.getException()); // 실패했을 경우. 이하 상품 코드 동일
-                }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(Store_RecommendList.this,"something went wrong",Toast.LENGTH_SHORT).show();
             }
         });
-// 두번째 상품
-        DocumentReference goods2 = db.collection("StoreGoods").document("brAE7c0BTuhQqInLY73s");
-        goods2.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-                        String storeName2 = (String) document.get("StoreName");
-                        store2.setText(storeName2);
-                        String goodsTitle2 = (String) document.get("GoodsTitle");
-                        title2.setText(goodsTitle2);
-                        String goodsReview2 = (String) document.get("GoodsReview");
-                        review2.setText(goodsReview2);
-                        String goodsPrice2 = (String) document.get("GoodsPrice");
-                        price2.setText(goodsPrice2);
-                    } else {
-                        Log.d(TAG, "No such document");
-                    }
-                } else {
-                    Log.d(TAG, "get failed with ", task.getException());
-                }
-            }
-        });
-// 세번째 상품
-        DocumentReference goods3 = db.collection("StoreGoods").document("ki4XlFCvbFUHyh92v0oT");
-        goods3.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-                        String storeName3 = (String) document.get("StoreName");
-                        store3.setText(storeName3);
-                        String goodsTitle3 = (String) document.get("GoodsTitle");
-                        title3.setText(goodsTitle3);
-                        String goodsReview3 = (String) document.get("GoodsReview");
-                        review3.setText(goodsReview3);
-                        String goodsPrice3 = (String) document.get("GoodsPrice");
-                        price3.setText(goodsPrice3);
-                    } else {
-                        Log.d(TAG, "No such document");
-                    }
-                } else {
-                    Log.d(TAG, "get failed with ", task.getException());
-                }
-            }
-        });
-// 네번째 상품
-        DocumentReference goods4 = db.collection("StoreGoods").document("p0hx6kpCb7wVorH4xKoK");
-        goods4.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-                        String storeName4 = (String) document.get("StoreName");
-                        store4.setText(storeName4);
-                        String goodsTitle4 = (String) document.get("GoodsTitle");
-                        title4.setText(goodsTitle4);
-                        String goodsReview4 = (String) document.get("GoodsReview");
-                        review4.setText(goodsReview4);
-                        String goodsPrice4 = (String) document.get("GoodsPrice");
-                        price4.setText(goodsPrice4);
-                    } else {
-                        Log.d(TAG, "No such document");
-                    }
-                } else {
-                    Log.d(TAG, "get failed with ", task.getException());
-                }
-            }
-        });
-// 다섯번째 상품
-        DocumentReference goods5 = db.collection("StoreGoods").document("w6XoWmhY0t4PBFq8FJ9q");
-        goods5.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                if (task.isSuccessful()) {
-                    DocumentSnapshot document = task.getResult();
-                    if (document.exists()) {
-                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
-                        String storeName5 = (String) document.get("StoreName");
-                        store5.setText(storeName5);
-                        String goodsTitle5 = (String) document.get("GoodsTitle");
-                        title5.setText(goodsTitle5);
-                        String goodsReview5 = (String) document.get("GoodsReview");
-                        review5.setText(goodsReview5);
-                        String goodsPrice5 = (String) document.get("GoodsPrice");
-                        price5.setText(goodsPrice5);
-                    } else {
-                        Log.d(TAG, "No such document");
-                    }
-                } else {
-                    Log.d(TAG, "get failed with ", task.getException());
-                }
-            }
-        });
-    }}
+    }
+}
 
 
 
---
+++ app/src/main/java/com/example/plant01/store/store_GoodsAdapter2.java
@@ -0,0 +1,65 @@
+package com.example.plant01.store;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.plant01.R;
+
+import java.util.List;
+
+public class store_GoodsAdapter2 extends RecyclerView.Adapter<store_GoodsAdapter2.store_viewholder> {
+
+    private Store_RecommendList activity;
+    private List<StoreGoods> goodsList;
+
+    public store_GoodsAdapter2(Store_RecommendList activity, List<StoreGoods> goodsList){
+        this.activity = activity;
+        this.goodsList = goodsList;
+    }
+
+    @NonNull
+    @Override
+    public store_viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(activity).inflate(R.layout.regoods, parent,false);
+        return new store_viewholder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull store_viewholder holder, int position) {
+        Glide.with(holder.itemView)
+                .load(goodsList.get(position).getGoodsImg())
+                .into(holder.img0);
+        holder.store0.setText(goodsList.get(position).getStoreName());
+        holder.title0.setText(goodsList.get(position).getGoodsTitle());
+        holder.review0.setText(goodsList.get(position).getGoodsReview());
+        holder.price0.setText(goodsList.get(position).getGoodsPrice());
+    }
+
+    @Override
+    public int getItemCount() {
+        return goodsList.size();
+    }
+
+    public static class store_viewholder extends RecyclerView.ViewHolder{
+
+        ImageButton img0;
+        TextView store0, title0, review0, price0;
+
+        public store_viewholder(@NonNull View itemView) {
+            super(itemView);
+
+            img0 = itemView.findViewById(R.id.recommend_img0);
+            store0 = itemView.findViewById(R.id.recommend_store0);
+            title0 = itemView.findViewById(R.id.recommend_title0);
+            review0 = itemView.findViewById(R.id.recommend_review0);
+            price0 = itemView.findViewById(R.id.recommend_price0);
+        }
+    }
+}

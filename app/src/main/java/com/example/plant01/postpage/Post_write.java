4198690245211624fcac9b4b1b029ad6d1658d17
cheong <rhrnak0708@g.shingu.ac.kr>
2021-12-06 04:33:11 +0900

    이미지 크기 ..?

--- app/src/main/java/com/example/plant01/postpage/Post_write.java
+++ app/src/main/java/com/example/plant01/postpage/Post_write.java
@@ -1,8 +1,11 @@
 package com.example.plant01.postpage;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,16 +15,25 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.plant01.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.UUID;
@@ -39,8 +51,8 @@ public class Post_write extends AppCompatActivity {
     private Uri galleryUri;
     private byte[] data1;
     private ImageView uploadimg;
-    String postid;
-
+//    String postid;
+    String postID = UUID.randomUUID().toString();
 
     @Override
     public void onBackPressed() {
@@ -101,16 +113,20 @@ public class Post_write extends AppCompatActivity {
         Post.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
                 String title = Title.getText().toString();
                 String contents = Contents.getText().toString();
-                String id = UUID.randomUUID().toString();
-                String board =  spinner.getSelectedItem().toString();
+//                String userid
+                String id = user.getUid();
+                String board = spinner.getSelectedItem().toString();
                 Date timestamp = Timestamp.now().toDate();
+                String contentimg = null;
 
-                saveToFireStore(id, title, contents, board, timestamp);
+                saveToFireStore(id, title, contents, board, timestamp, contentimg);
             }
-            private void saveToFireStore(String userid, String title, String contents, String board, Date timestamp) {
-                String postID = UUID.randomUUID().toString();
+
+            private void saveToFireStore(String userid, String title, String contents, String board, Date timestamp, String contentimg) {
+
                 if (!title.isEmpty() && !contents.isEmpty()) {
                     HashMap<String, Object> map = new HashMap<>();
                     map.put("userID", userid);
@@ -118,6 +134,7 @@ public class Post_write extends AppCompatActivity {
                     map.put("content", contents);
                     map.put("board", board);
                     map.put("postDate", timestamp);
+                    map.put("contentImg", contentimg);
 
                     db.collection("Post").document(postID).set(map)
                             .addOnCompleteListener(new OnCompleteListener<Void>() {
@@ -125,6 +142,7 @@ public class Post_write extends AppCompatActivity {
                                 public void onComplete(@NonNull Task<Void> task) {
                                     if (task.isSuccessful()) {
                                         Toast.makeText(Post_write.this, "Post Data!!", Toast.LENGTH_SHORT).show();
+                                        update();
                                     }
                                 }
                             }).addOnFailureListener(new OnFailureListener() {
@@ -137,6 +155,63 @@ public class Post_write extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+//            profilePath = data.getStringExtra("profilePath");
+            //파이어스토리지와 연결
+            uploadimg.setVisibility(View.VISIBLE);
+            galleryUri = data.getData();
+
+//            Bitmap bitmap = (Bitmap) data.getParcelableExtra("data");
+//            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+//            data1 = baos.toByteArray();
+
+            try {
+                InputStream in = getContentResolver().openInputStream(data.getData());
+                Bitmap bitmap = BitmapFactory.decodeStream(in);
+                in.close();
+                uploadimg.setImageBitmap(bitmap);
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+    private void update() {
+//        profileImageView.setImageBitmap(bitmap);
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+//            profileImageView = (RoundedImageView) findViewById(R.id.img_setting_user);
+        //현재 유저 받아오기
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        //파이어스토어와 연결
+        db = FirebaseFirestore.getInstance();
+        StorageReference storageRef = storage.getReference();
+        StorageReference mountainImagesRef = storageRef.child("PostImg/"+user.getUid()+"/profile.jpg");
+        UploadTask uploadTask = mountainImagesRef.putFile(galleryUri);
+        Log.e("data1", galleryUri.toString());
+        mountainImagesRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Log.e("이미지주소", uri.toString());
+                db.collection("Post").document(postID)
+                        .update("contentImg", uri.toString());
+            }
+        });
+
+    }
+}
 
 
 
@@ -203,8 +278,8 @@ public class Post_write extends AppCompatActivity {
 //        Video.setOnClickListener(cl);
 //        Post.setOnClickListener(cl);
 //        back.setOnClickListener(cl);
-    }
-}
+//    }
+//}
 
 
 
